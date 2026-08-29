12e35a3754fc27d22b3dd931efeb0f548bd2f14d
marcel <taubert.marcel@gmail.com>
2026-08-29 11:24:46 +0100

    fix(timer): clobber `a1` register after SBI call

--- src/timer.rs
+++ src/timer.rs
@@ -1,6 +1,4 @@
-use core::arch::asm;
-
-use crate::riscv;
+use crate::{device_tree, riscv};
 
 /// Helper function that creates a timer for `secs` second using the frequency read from the device
 /// tree.
@@ -8,14 +6,15 @@ use crate::riscv;
 /// NOTE: Since this is reading values from the device tree it has to be initialized, even in
 /// tests.
 pub fn new_time(secs: usize) {
-    // TODO(mt): create a mapping for functions in sbi module.
     const TIME_FN: usize = 0x54494D45;
 
-    let freq = crate::device_tree::timer_frequency();
+    let freq = device_tree::timer_frequency();
     let time = freq * secs;
 
+    let error: usize;
+
     unsafe {
-        asm!(
+        core::arch::asm!(
             "rdtime t0",
             "add a0, t0, t1",
             "li a6, 0x0",
@@ -23,17 +22,22 @@ pub fn new_time(secs: usize) {
             in("t1") time,
             in("a7") TIME_FN,
             out("t0") _,
-            out("a0") _,
             out("a6") _,
+            lateout("a0") error,
+            lateout("a1") _,
         )
     }
+
+    // The [spec](https://docs.riscv.org/reference/sbi/v3.0/ext-time.html) defines `error` is
+    // always 0.
+    debug_assert_eq!(error, 0);
 }
 
 pub fn uptime() -> usize {
-    riscv::asm::rdtime() / crate::device_tree::timer_frequency()
+    riscv::asm::rdtime() / device_tree::timer_frequency()
 }
 
 pub fn uptime_ms() -> usize {
-    let freq = crate::device_tree::timer_frequency() / 1000;
+    let freq = device_tree::timer_frequency() / 1000;
     riscv::asm::rdtime() / freq
 }
